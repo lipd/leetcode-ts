f03d0b5bf3a2b7d80d427029bfb4e60d9648b788
lipd <lipeidao@gmail.com>
2020-07-12 14:29:47 +0800

    update 46

---
+++ src/46-permutations/1-backtrack.ts
@@ -0,0 +1,20 @@
+// backtrack
+
+function permute(nums: number[]): number[][] {
+  const res: number[][] = [];
+  function generate(curr: number[], recorder: {[p: string]: number}) {
+    if (curr.length === nums.length) return res.push(curr.slice());
+    nums.map(each => {
+      if (recorder[each]) return;
+
+      curr.push(each);
+      recorder[each] = 1
+      generate(curr, recorder);
+
+      curr.pop()
+      delete recorder[each];
+    });
+  }
+  generate([], {});
+  return res;
+};
